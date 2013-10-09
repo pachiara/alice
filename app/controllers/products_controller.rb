75de08f965a9bbc78bf76295378d78e69468fb67
EnzoFerri <enzo.ferri@lispa.it>
2013-10-09 11:25:56 +0200

    Nome del pdf grafico generato reso corrispondente al nome del prodotto cui si riferisce.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -104,7 +104,7 @@ class ProductsController < ApplicationController
         
     respond_to do |format|
       format.pdf do
-        render :pdf => "graphics",
+        render :pdf => @product.name.gsub(' ', '_'),
                :header => { :left => 'Alice',
                             :center => @title,
                             :right => t('actions.messages.classification'),
