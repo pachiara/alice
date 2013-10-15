111bdd21ae18b3daa4bbef26778f51c07294c417
EnzoFerri <enzo.ferri@lispa.it>
2013-10-15 16:56:11 +0200

    Calcolo automatico numero di colonne della legenda

--- app/assets/javascripts/donut.js
+++ app/assets/javascripts/donut.js
@@ -1,18 +1,14 @@
   function donut (dataset, selector, hole_text) {
     donut_box_side  = typeof donut_box_side  !== 'undefined' ? donut_box_side : 250;
-    donut_radius    = donut_box_side / 2;
     donut_font_size = typeof donut_font_size !== 'undefined' ? donut_font_size : "18px";
+    donut_radius    = donut_box_side / 2;
         
-    legend_font_size     = typeof legend_font_size     !== 'undefined' ? legend_font_size     : "14px";
-    legend_translate     = typeof legend_translate     !== 'undefined' ? legend_translate     : 20;
-    legend_rect_side     = typeof legend_rect_side     !== 'undefined' ? legend_rect_side     : 18;
-    legend_dy            = typeof legend_dy            !== 'undefined' ? legend_dy            : ".35em";
-    legend_text_x        = typeof legend_text_x        !== 'undefined' ? legend_text_x        : 24;
-    legend_text_y        = typeof legend_text_y        !== 'undefined' ? legend_text_y        : 9;
-    legend_rows_x_column = typeof legend_rows_x_column !== 'undefined' ? legend_rows_x_column : 20;
-    legend_width         = typeof legend_width         !== 'undefined' ? legend_width         : 200;
-    legend_height        = typeof legend_height        !== 'undefined' ? legend_height        : donut_box_side;
-    legend_padding       = typeof legend_padding       !== 'undefined' ? legend_padding       : 5;
+    legend_width          = typeof legend_width         !== 'undefined' ? legend_width         : 200;
+    legend_height         = typeof legend_height        !== 'undefined' ? legend_height        : donut_box_side;
+    legend_font_size      = typeof legend_font_size     !== 'undefined' ? legend_font_size     : "14px";
+    legend_rect_side      = typeof legend_rect_side     !== 'undefined' ? legend_rect_side     : 18;
+    legend_translate      = legend_rect_side + 2;
+    legend_padding        = 5;
 
     arc = d3.svg.arc()
       .outerRadius(donut_radius)
@@ -59,8 +55,9 @@
     });
     color_components.domain(entries);
     
+    legend_rows_x_column = Math.floor(legend_height/legend_translate);
     legend_columns = Math.ceil(entries.length / legend_rows_x_column);
-    legend_column_width = legend_width / legend_columns - legend_padding;
+    legend_column_width = legend_width / legend_columns;
      
     legend = d3.select(selector).append("svg")
         .attr("class", "legend")
@@ -79,10 +76,8 @@
         .style("fill", color_components);
   
     legend.append("text")
-        .attr("x", legend_text_x)
-        .attr("y", legend_text_y)
-        .attr("dy", legend_dy)
+        .attr("dy", "1em")
+        .attr("dx", (legend_rect_side + 2))
         .attr("font-size", legend_font_size)
         .text(function(d) { return d; });
-    
   }
