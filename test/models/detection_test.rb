315845b4ad2385f23eb3a14d8d7fc92700a1c2a4
Chiara Palmieri <chiara.palmieri@gmail.com>
2014-04-17 17:33:38 +0200

    test

--- test/models/detection_test.rb
+++ test/models/detection_test.rb
@@ -4,4 +4,32 @@ class DetectionTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  
+  def testPost
+    url = URI.parse('http://localhost:3000/detection/')
+    request = Net::HTTP::Post.new(url.path)
+    request.body = "<?xml version='1.0' encoding='UTF-8' standalone='no'?>
+    <licenseSummary>
+    <dependencies>
+    <dependency>
+      <groupId>commons-logging</groupId>
+      <artifactId>commons-logging</artifactId>
+      <version>1.1.1</version>
+      <licenses>
+        <license>
+          <name>The Apache Software License, Version 2.0</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+    </dependency>
+    </dependencies>
+    </licenseSummary>"
+    
+    response = Net::HTTP.start(url.host, url.port) {|http| http.request(request)}
+    assert_equal '201 Created', response.get_fields('Status')[0]
+  end
+
+  
 end
