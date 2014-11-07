55187f8c8841b0be848f27952f0df6d30a298557
pachiara <chiara.palmieri@gmail.com>
2014-11-07 17:25:37 +0100

    Modifiche per Bootstrap 3.2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap
+//= require bootstrap-sprockets
 //= require moment
 //= require moment/it
 //= require bootstrap-datetimepicker
