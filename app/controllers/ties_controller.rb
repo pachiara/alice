34462785c55e7cf20eb83bf475437a247b0d5609
EnzoFerri <enzo.ferri@lispa.it>
2015-05-29 15:48:35 +0200

    Primo allineamento gestione ties

--- app/controllers/ties_controller.rb
+++ app/controllers/ties_controller.rb
@@ -17,15 +17,19 @@ class TiesController < ApplicationController
     @title = t('actions.listing') + " " + t('activerecord.models.ties')
     @search_form_path = ties_index_path
         
-    if !params[:product_id].nil?
-      @product = Product.find(params[:product_id])
-      @components = @product.components.order('name, version').paginate(page: params[:component_page], per_page: 10)   
-      @products = Product.order('name, version').page(params[:page]).per_page(10)
+    if !params[:release_id].nil?
+      @release = Release.find(params[:release_id])
+#      @components = @product.components.order('name, version').paginate(page: params[:component_page], per_page: 10)   
+      @components = @release.components.order('name').paginate(page: params[:component_page], per_page: 10)   
+#      @products = Product.order('name, version').page(params[:page]).per_page(10)
+#TODO rivedere criterio selezione/ordinamento: tutte le release o una per prodotto (ultima)?
+      @products = Product.order('name').page(params[:page]).per_page(10)
     end
 
     if !params[:component_id].nil?  
       @component = Component.find(params[:component_id])
-      @products = @component.products.order('name, version').paginate(page: params[:page], per_page: 10)   
+#      @products = @component.products.order('name, version').paginate(page: params[:page], per_page: 10)   
+      @releases = @component.releases.order('version_name').paginate(page: params[:page], per_page: 10)   
       @components = Component.order('name, version').page(params[:component_page]).per_page(10)
     end
 
@@ -38,33 +42,33 @@ class TiesController < ApplicationController
   def edit
     @title = t('actions.edit') + " " + t('activerecord.models.tie')
     require 'will_paginate/array'
-    @search_form_path = product_ties_edit_path
-    @product = Product.find(params[:product_id])
+    @search_form_path = release_ties_edit_path
+    @release = Release.find(params[:release_id])
     
     if !params[:component_del].nil?
-      @product.del_relation(params[:component_del])
+      @release.del_relation(params[:component_del])
     end
     if !params[:component_add].nil?
-      @product.add_relation(params[:component_add])
+      @release.add_relation(params[:component_add])
     end
 
-    @components_ties = @product.components.order('name, version').paginate(page: params[:component_ties_page], per_page: 6)
-    @components = (Component.order('name, version').where("name like ?", "%#{params[:component_name]}%") - @product.components.order('name, version')).paginate(page: params[:component_page], per_page: 6)
+    @components_ties = @release.components.order('name, version').paginate(page: params[:component_ties_page], per_page: 6)
+    @components = (Component.order('name, version').where("name like ?", "%#{params[:component_name]}%") - @release.components.order('name, version')).paginate(page: params[:component_page], per_page: 6)
          
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @product }
+      format.json { render json: @release }
     end    
   end    
   
   # DELETE /product/1/ties/destroy
   # DELETE /product/1/ties/destroy.json
   def destroy
-    @product = Product.find(params[:product_id])
-    @product.delete_components
+    @release = Release.find(params[:release_id])
+    @release.delete_components
 
     respond_to do |format|
-      format.html { redirect_to product_ties_edit_url }
+      format.html { redirect_to release_ties_edit_url }
       format.json { head :no_content }
     end
   end
