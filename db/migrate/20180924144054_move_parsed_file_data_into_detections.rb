3e146ff96580183fb8dcbc3d466d8ac9c53f55ea
Enzo Ferri <enzo.ferri@lispa.it>
2018-10-09 16:10:16 +0200

    patch migrazione detections

--- db/migrate/20180924144054_move_parsed_file_data_into_detections.rb
+++ db/migrate/20180924144054_move_parsed_file_data_into_detections.rb
@@ -5,11 +5,17 @@ class MoveParsedFileDataIntoDetections < ActiveRecord::Migration[5.2]
       detection.parsed_file_at = detection.xml_updated_at
       detection.save 
     end
-    remove_attachment :detections, :xml
+    remove_column :detections, :xml_file_name
+    remove_column :detections, :xml_updated_at
+    remove_column :detections, :xml_content_type
+    remove_column :detections, :xml_file_size
   end
  
   def down
-    add_attachment :detections, :xml
+      add_column :detections, :xml_file_name, :string
+      add_column :detections, :xml_updated_at, :datetime
+      add_column :detections, :xml_content_type, :string
+      add_column :detections, :xml_file_size, :integer
     Detection.all.each do |detection|
       detection.xml_file_name = detection.parsed_file_name
       detection.xml_updated_at = detection.parsed_file_at
