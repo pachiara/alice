a21a45fc5a72068148ffa9d02873da40d1defec7
Enzo Ferri <enzo.ferri@lispa.it>
2020-09-25 10:28:32 +0200

    Configurazione time-zone

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module Alice
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-  
+ 
+    config.time_zone = "Rome" 
     config.active_record.default_timezone = :local  
     
     # Settings in config/environments/* take precedence over those specified here.
